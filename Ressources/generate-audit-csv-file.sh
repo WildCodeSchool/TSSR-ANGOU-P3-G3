def97e46814477d1f954d38e861ef8ed22458697
Matth <84233618+Tr3n4rT@users.noreply.github.com>
2025-03-26 15:39:35 +0100

    Add files via upload

---
+++ Ressources/generate-audit-csv-file.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+# Permet de générer un fichier CSV à partir des fichiers généré pour un audit de sécurité effectué avec Lynis sur un serveur Linux.
+# Utilise le fichier log à l'emplacement : /var/log/lynis.log
+
+# Emplacement du fichier
+log="/var/log/lynis.log"
+
+# Crée l'entête du fichier CSV. Changer les semi-colons par un autre séparateur si besoin.
+echo "AUDIT EFFECTUE PAR;NOM DU SERVEUR;EMPLACEMENT DU SERVEUR;
+Mathieu Leroux;$HOSTANME;FALKENSTEIN - Virtualisé sur Proxmox;
+DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION" > Audit-$HOSTANME-.csv 
+
+# Formate le fichier log en csv
+cat $log | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}' >> Audit-$HOSTANME-.csv
