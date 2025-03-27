2b43e50d8879710fd36ea323a6eae8cddc9f7b76
Matth <84233618+Tr3n4rT@users.noreply.github.com>
2025-03-27 12:21:57 +0100

    Update generate-audit-csv-file.sh

--- Ressources/generate-audit-csv-file.sh
+++ Ressources/generate-audit-csv-file.sh
@@ -10,7 +10,14 @@ log="/var/log/lynis.log"
 echo "AUDIT EFFECTUE PAR;NOM DU SERVEUR;EMPLACEMENT DU SERVEUR;SCORE DE L'AUDIT;SCORE DE REVISION;
 Mathieu Leroux;$HOSTNAME;FALKENSTEIN - Virtualisé sur Proxmox;;;
 
-DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION;STATUS DE CORRECTION;COMMENTAIRE;" > Audit-$HOSTNAME.csv 
+" > Audit-$HOSTNAME.csv 
 
-# Formate le fichier log en csv
+# Crée un résumé et un ligne de l'audit en insérant les lignes contenant les mots clés Warning et Suggestion au début du fichier
+echo "RESUME DE L'AUDIT;
+DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION;COMMENTAIRE / CORRECTION;STATUS DE CORRECTION;" >> Audit-$HOSTNAME.csv
+grep Warning
+
+# Formate l'intégralité du fichier log en csv et l'ajoute à la fin du fichier csv
+echos "AUDIT COMPLET;
+DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION;" >> Audit-$HOSTNAME.csv
 cat $log | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}' >> Audit-$HOSTNAME.csv
