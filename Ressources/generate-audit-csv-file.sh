e4f1a9b57377fe60670c4b86c6afce9da729694c
Matth <84233618+Tr3n4rT@users.noreply.github.com>
2025-03-27 17:47:18 +0100

    Update generate-audit-csv-file.sh

--- Ressources/generate-audit-csv-file.sh
+++ Ressources/generate-audit-csv-file.sh
@@ -9,17 +9,18 @@ log="/var/log/lynis.log"
 # Crée l'entête du fichier CSV. Changer les semi-colons par un autre séparateur si besoin.
 echo "AUDIT EFFECTUE PAR;NOM DU SERVEUR;EMPLACEMENT DU SERVEUR;SCORE DE L'AUDIT;SCORE DE REVISION;
 Mathieu Leroux;$HOSTNAME;FALKENSTEIN - Virtualisé sur Proxmox;;;
-
 " > Audit-$HOSTNAME.csv 
 
 # Crée résumé l'audit en insérant les lignes contenant les mots clés Warning et Suggestion au début du fichier
 echo "RESUME DE L'AUDIT;
 WARNING;
 DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION;COMMENTAIRE / CORRECTION;STATUS DE CORRECTION;" >> Audit-$HOSTNAME.csv
-cat $log | grep "Warning" | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}' >> Audit-$HOSTNAME.csv
+cat $log | grep "Warning" | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}
+' >> Audit-$HOSTNAME.csv
 echo "SUGGESTION;
 DATE;HEURE;TYPE DE MESSAGE;DESCRIPTION;COMMENTAIRE / CORRECTION;STATUS DE CORRECTION;" >> Audit-$HOSTNAME.csv
-cat $log | grep "Suggestion" | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}' >> Audit-$HOSTNAME.csv
+cat $log | grep "Suggestion" | awk '{print $1";"$2";"$3";"substr($0, index($0,$4))}
+' >> Audit-$HOSTNAME.csv
 
 # Formate l'intégralité du fichier log en csv et l'ajoute à la fin du fichier csv
 echos "AUDIT COMPLET;
